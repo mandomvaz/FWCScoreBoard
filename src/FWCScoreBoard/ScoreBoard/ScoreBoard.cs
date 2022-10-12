0b6a876741648780a91b45074379c3c9d709fc15
Manuel Domínguez Vázquez <mandomvaz@gmail.com>
2022-10-12 11:37:17 +0200

    StartGameReturnIntegerId Test Green

--- src/FWCScoreBoard/ScoreBoard/ScoreBoard.cs
+++ src/FWCScoreBoard/ScoreBoard/ScoreBoard.cs
@@ -2,21 +2,27 @@
 {
     public class ScoreBoard
     {
-        private List<string> Games { get; set; }
+        private Dictionary<int, string> Games { get; set; }
+        private int CurrentId { get; set; }
 
         public ScoreBoard()
         {
-            Games = new List<string>();
+            Games = new Dictionary<int, string>();
+            CurrentId = 0;
         }
 
-        public void StartGame(string homeTeam, string awayTeam)
+        public int StartGame(string homeTeam, string awayTeam)
         {
             if(String.IsNullOrWhiteSpace(homeTeam) || String.IsNullOrWhiteSpace(awayTeam))
             {
                 throw new ArgumentException();
             }
 
-            Games.Add($"{homeTeam} 0 - {awayTeam} 0");
+            CurrentId += 1;
+
+            Games.Add(CurrentId, $"{homeTeam} 0 - {awayTeam} 0");
+
+            return CurrentId;
         }
     }
 }
