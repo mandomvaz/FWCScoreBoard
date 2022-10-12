0fbcfac1205e8e3ea714e61247bb3590bbd67bcb
Manuel Domínguez Vázquez <mandomvaz@gmail.com>
2022-10-12 10:59:48 +0200

    CanStartGame Test Green

--- src/FWCScoreBoard/ScoreBoard/ScoreBoard.cs
+++ src/FWCScoreBoard/ScoreBoard/ScoreBoard.cs
@@ -2,6 +2,16 @@
 {
     public class ScoreBoard
     {
+        private List<string> Games { get; set; }
 
+        public ScoreBoard()
+        {
+            Games = new List<string>();
+        }
+
+        public void StartGame(string homeTeam, string awayTeam)
+        {
+            Games.Add($"{homeTeam} 0 - {awayTeam} 0");
+        }
     }
 }
